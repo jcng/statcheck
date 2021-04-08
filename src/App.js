865d4b1600eb90db6e0bc35a769b06d91bd613c2
jcng <joshuang@Joshuas-MacBook-Pro.local>
2021-04-07 23:14:58 -0700

    Initial commit with test data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+// local JSON for testing purposes
+var testDataRaw = require('./data/Lucian.json');
+var testData = testDataRaw.data[Object.keys(testDataRaw.data)[0]]
+console.log(testData);
+
+var champSplash = `//ddragon.leagueoflegends.com/cdn/img/champion/splash/${testData.name}_0.jpg`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="champ-hero-splash">
+        <img src={champSplash}></img>
+      </div>
+      <h1 className="champ-title">{testData.id}</h1>
     </div>
   );
 }
