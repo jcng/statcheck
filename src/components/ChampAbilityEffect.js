c84e085f09889ea01488cfdb290414171f51eb6f
jcng <joshuang.contact@gmail.com>
2021-06-05 22:59:13 -0700

    Begin adding champ ability effect logic

--- src/components/ChampAbilityEffect.js
+++ src/components/ChampAbilityEffect.js
@@ -1,9 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import ChampAbilityLeveling from './ChampAbilityLeveling'
 
 class ChampAbilityEffect extends Component {
+    abilityLevelingWrapper() {
+        if (this.props.effectData.leveling) {
+            const levelingList = this.props.effectData.leveling.map((attrib) =>
+                <ChampAbilityLeveling levelingDataRaw={attrib} key={attrib.attribute} ></ChampAbilityLeveling>
+            );
+            return (
+                <div className="champ-ability-effect-leveling-wrapper">
+                    {levelingList}
+                </div>
+            )
+        }
+    }
+
     render() {
         return (
-            <div className="champ-ability-effect-description">{this.props.effectData.description}</div>
+            <div className="champ-ability-effect">
+                <div className="champ-ability-effect-description">{this.props.effectData.description}</div>
+                {this.abilityLevelingWrapper()}
+            </div>
         )
     }
 }
